e30cf5e2c4f25649974ba4c4a5e4b5246d64ef66
Roman Baygildin <rbaygildin95@gmail.com>
2018-02-26 03:02:12 +0300

    View tags

--- neurdicom_client/src/services/DicomService.js
+++ neurdicom_client/src/services/DicomService.js
@@ -3,7 +3,7 @@ import * as queryString from 'query-string';
 const PATIENTS_ROOT_URL = '/api/patients';
 const STUDIES_ROOT_URL = '/api/studies';
 const SERIES_ROOT_URL = '/api/series';
-const INSTANCES_ROOT_URL = '/api/instance';
+const INSTANCES_ROOT_URL = '/api/instances';
 
 export default class DicomService {
     static findPatients(f, params = {}) {
@@ -128,6 +128,23 @@ export default class DicomService {
     static findInstancesBySeriesId(seriesId, f) {
         fetch(
             `${SERIES_ROOT_URL}/${seriesId}/instances`
+        ).then(function (response) {
+            if (response.status >= 200 && response.status < 300) {
+                return response;
+            }
+            console.log(response.status);
+            const error = new Error(`HTTP Error ${response.statusText}`);
+            error.status = response.statusText;
+            error.response = response;
+            throw error;
+        }).then(response => {
+            return response.json();
+        }).then(f);
+    }
+
+    static findTagsByInstanceId(instanceId, f) {
+        fetch(
+            `${INSTANCES_ROOT_URL}/${instanceId}/tags`
         ).then(function (response) {
             if (response.status >= 200 && response.status < 300) {
                 return response;
